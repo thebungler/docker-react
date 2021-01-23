21b50ee48cebfe0b567dbe978317c843469f8c96
Franjo <inbox.horvat@gmail.com>
2021-01-23 12:56:18 +0100

    main page change

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>Hello World! How are you?</p>
+        <p>This is the <b>main</b> branch</p>
         <a
           className="App-link"
           href="https://reactjs.org"
